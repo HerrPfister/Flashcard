8f82f605a11fa7ddf50ee601bf035401f76053cc
Matthew_Pfister <matthew.pfister@slalom.com>
2021-02-21 22:16:05 -0600

    Adding remaining stories to storybook

--- src/Flashcard/Flashcard.stories.tsx
+++ src/Flashcard/Flashcard.stories.tsx
@@ -16,6 +16,27 @@ Playground.args = {
   disabled: false,
 };
 
+Playground.argTypes = {
+  back: { description: 'Content to be rendered on the back side of the flashcard.' },
+  front: { description: 'Content to be rendered on the front side of the flashcard.' },
+  FrontSideProps: {
+    description: `These are the properties passed to the front side paper component.<br/><br/>
+      **elevation:** will change the shadow depth, corresponds to dp. It accepts values between 0 and 24 inclusive..<br/>
+      **variant:** will change the rendered style of the paper component. Accepts elevation or outlined.<br/>
+      **square:** if true rounded corners are removed.<br/>
+      [See the material ui paper docs](https://material-ui.com/components/paper)`,
+  },
+  BackSideProps: {
+    description: `These are the properties passed to the back side paper component.<br/><br/>
+      **elevation:** will change the shadow depth, corresponds to dp. It accepts values between 0 and 24 inclusive..<br/>
+      **variant:** will change the rendered style of the paper component. Accepts elevation or outlined.<br/>
+      **square:** if true rounded corners are removed.<br/>
+      [See the material ui paper docs](https://material-ui.com/components/paper)`,
+  },
+  startFlipped: { description: 'If set to true the card will be rendered back side up.' },
+  disabled: { description: 'If set to true the cards flipping functionality will be disabled.' },
+};
+
 export const DefaultStory: Story<FlashcardProps> = () => <Flashcard back="Side B" front="Side A" />;
 
 DefaultStory.storyName = 'Default';
@@ -24,3 +45,11 @@ export default {
   title: 'Layout/Flashcard',
   component: DefaultStory,
 };
+
+export const FlippedStory: Story<FlashcardProps> = () => <Flashcard back="Side B" front="Side A" startFlipped={true} />;
+
+FlippedStory.storyName = 'Flipped';
+
+export const DisabledStory: Story<FlashcardProps> = () => <Flashcard back="Side B" front="Side A" disabled={true} />;
+
+DisabledStory.storyName = 'Disabled';
